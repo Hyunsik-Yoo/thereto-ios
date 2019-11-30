6f5da9e0314412fdb7919bb60d36ec58d31233fd
Yoo Hyun Sik <dbgustlr92@gmail.com>
2019-11-30 14:39:22 +0900

    Base 클래스 정의

---
+++ thereto-ios/thereto-ios/features/base/BaseVC.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class BaseVC: UIViewController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
