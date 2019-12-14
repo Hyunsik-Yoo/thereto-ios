a5e326541c12114e02226898dd43c67b1315954f
Yoo Hyun Sik <dbgustlr92@gmail.com>
2019-12-15 00:58:56 +0900

    AlertUtil, FirebaseUtil 함수들 추가

--- thereto-ios/thereto-ios/features/util/AlertUtil.swift
+++ thereto-ios/thereto-ios/features/util/AlertUtil.swift
@@ -2,25 +2,28 @@ import UIKit
 
 struct AlertUtil {
     
-    static func show(vc: UIViewController, title: String? = nil, message: String? = nil) {
+    static func show(_ title: String? = nil, message: String? = nil) {
         let okAction = UIAlertAction(title: "확인", style: .default)
         
-        show(vc: vc, title: nil, message: message, [okAction])
+        show(title: nil, message: message, [okAction])
     }
     
-    static func showWithCancel(vc: UIViewController, title: String? = nil, message: String? = nil) {
+    static func showWithCancel(title: String? = nil, message: String? = nil) {
         let okAction = UIAlertAction(title: "확인", style: .default)
         let cancelAction = UIAlertAction(title: "취소", style: .cancel)
         
-        show(vc: vc, title: title, message: message, [okAction, cancelAction])
+        show(title: title, message: message, [okAction, cancelAction])
     }
     
-    static func show(vc: UIViewController, title: String?, message: String?, _ actions: [UIAlertAction]) {
-        let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        for action in actions {
-            controller.addAction(action)
+    static func show(title: String?, message: String?, _ actions: [UIAlertAction]) {
+        if let appDelegate = UIApplication.shared.delegate,
+            let rootVC = appDelegate.window??.rootViewController {
+            let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            
+            for action in actions {
+                controller.addAction(action)
+            }
+            rootVC.present(controller, animated: true)
         }
-        vc.present(controller, animated: true)
     }
 }
