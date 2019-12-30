d00fc36251a317ccdf3781445c35f0c11f36e1e5
Yoo Hyun Sik <dbgustlr92@gmail.com>
2019-12-30 13:42:46 +0900

    Drawer에 친구요청 관리 버튼 추가

--- thereto-ios/thereto-ios/features/shared/DrawerView.swift
+++ thereto-ios/thereto-ios/features/shared/DrawerView.swift
@@ -70,11 +70,24 @@ class DrawerView: BaseView {
         return label
     }()
     
+    let friendControllBtn = UIButton().then {
+        $0.setTitle("친구요청 관리", for: .normal)
+        $0.setTitleColor(UIColor.init(r: 60, g: 46, b: 42), for: .normal)
+        $0.titleLabel?.font = UIFont.init(name: "SpoqaHanSans-Regular", size: 12)
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.init(r: 60, g: 46, b: 42).cgColor
+    }
+    
+    let newBadge = UIView().then {
+        $0.backgroundColor = UIColor.init(r: 255, g: 84, b: 41)
+        $0.layer.cornerRadius = 6
+    }
+    
     
     override func setup() {
         isUserInteractionEnabled = true
         addSubViews(bgView, closeBtn, logoImage, middleLine, letterboxLabel,
-                    sentletterLabel, friendLabel, setupLabel)
+                    sentletterLabel, friendLabel, setupLabel, friendControllBtn, newBadge)
     }
     
     override func bindConstraints() {
@@ -121,6 +134,19 @@ class DrawerView: BaseView {
         setupLabel.snp.makeConstraints { (make) in
             make.left.equalTo(letterboxLabel)
             make.top.equalTo(friendLabel.snp.bottom).offset(30)
+        }
+        
+        friendControllBtn.snp.makeConstraints { (make) in
+            make.bottom.equalToSuperview().offset(-44)
+            make.right.equalToSuperview().offset(-40)
+            make.width.equalTo(93)
+            make.height.equalTo(32)
+        }
+        
+        newBadge.snp.makeConstraints { (make) in
+            make.centerX.equalTo(friendControllBtn.snp.right)
+            make.centerY.equalTo(friendControllBtn.snp.top)
+            make.width.height.equalTo(12)
         }
     }
 }
