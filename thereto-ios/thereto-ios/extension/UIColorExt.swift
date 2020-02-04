b1619c2424860b9680a85181d4940bdc1cfc7a12
Yoo Hyun Sik <dbgustlr92@gmail.com>
2020-02-04 13:32:38 +0900

    친구 관리화면 Cell 생성

--- thereto-ios/thereto-ios/extension/UIColorExt.swift
+++ thereto-ios/thereto-ios/extension/UIColorExt.swift
@@ -9,4 +9,16 @@ extension UIColor {
     class var themeColor: UIColor? {
         return UIColor.init(r: 255, g: 248, b: 239)
     }
+    
+    class var orange_red: UIColor {
+        return UIColor.init(r: 255, g: 84, b: 41)
+    }
+    
+    class var black: UIColor {
+        return UIColor.init(r: 30, g: 30, b: 30)
+    }
+    
+    class var very_light_pink: UIColor {
+        return UIColor.init(r: 255, g: 248, b: 239)
+    }
 }
--- thereto-ios/thereto-ios/features/friend-controll/FriendTableVC.swift
+++ thereto-ios/thereto-ios/features/friend-controll/FriendTableVC.swift
@@ -12,9 +12,26 @@ class FriendTableVC: BaseVC {
         super.viewDidLoad()
         
         view = friendTableView
+        friendTableView.tableView.delegate = self
+        friendTableView.tableView.dataSource = self
+        friendTableView.tableView.register(FriendControlCell.self, forCellReuseIdentifier: FriendControlCell.registerId)
     }
     
     override func bindViewModel() {
         
+    }
+}
+
+extension FriendTableVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FriendControlCell.registerId, for: indexPath) as? FriendControlCell else {
+            return BaseTableViewCell()
+        }
+        
+        return cell
     }
 }
