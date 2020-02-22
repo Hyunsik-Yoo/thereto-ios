20ec09443f0f3c3132090c1033766f06b29c8682
Yoo Hyun Sik <dbgustlr92@gmail.com>
2020-02-22 13:16:22 +0900

    상세화면에서 삭제버튼 눌렀을 때, Alert뜨도록 적용

--- thereto-ios/thereto-ios/features/shared/letter-detail/LetterDetailVC.swift
+++ thereto-ios/thereto-ios/features/shared/letter-detail/LetterDetailVC.swift
@@ -27,7 +27,7 @@ class LetterDetailVC: BaseVC {
         
         letterDetailView.deleteBtn.rx.tap.bind { [weak self] in
             if let vc = self {
-                AlertUtil.showWithCancel(controller: vc, message: "삭제하시겠습니까?") {
+                AlertUtil.showWithCancel(title: nil, message: "삭제하시겠습니까?") {
                     if UserDefaultsUtil.isTutorialFinished() { // 튜토리얼일 경우 UserDefault 에서 설정
                         vc.deleteLetter(letterId: vc.letter.id)
                     } else {
