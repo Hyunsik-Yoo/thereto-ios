5d9b6b0ab9a1515eaf087074e71425d1a66223e3
Yoo Hyun Sik <dbgustlr92@gmail.com>
2020-06-14 13:31:01 +0900

    친구 상세화면 -> 편지쓰기 화면 진입시 받는사람에 상대방이름 안들어가있던 오류 수정

--- thereto-ios/thereto-ios/features/friends/friend-detail/FriendDetailViewModel.swift
+++ thereto-ios/thereto-ios/features/friends/friend-detail/FriendDetailViewModel.swift
@@ -1,88 +1,76 @@
 import Foundation
 import RxSwift
+import RxCocoa
 
 class FriendDetailViewModel: BaseViewModel {
     
-    var input: Input
-    var output: Output
+    let input = Input()
+    let output = Output()
     var userService: UserServiceProtocol
     var userDefaults: UserDefaultsProtocol
     
     struct Input {
-        var tapFavorite: AnyObserver<Void>
-        var tapDelete: AnyObserver<Void>
-        var tapWrite: AnyObserver<Void>
+        let tapFavorite = PublishSubject<Void>()
+        let tapDelete = PublishSubject<Void>()
+        let tapWrite = PublishSubject<Void>()
     }
     
     struct Output {
-        var enableFavorite: Observable<Bool>
-        var friend: Observable<Friend>
-        var popup: Observable<Void>
-        var showLoading: Observable<Bool>
-        var showAlerts: Observable<(String, String)>
+        let enableFavorite = PublishRelay<Bool>()
+        let popup = PublishRelay<Void>()
+        let friend = PublishRelay<Friend>()
+        let showLoading = PublishRelay<Bool>()
+        let showAlerts = PublishRelay<(String, String)>()
+        let goToWrite = PublishRelay<Friend>()
     }
     
-    let tapFavoritePublisher = PublishSubject<Void>()
-    let tapDeletePublisher = PublishSubject<Void>()
-    let tapWritePublisher = PublishSubject<Void>()
-    
-    let enableFavoritePublisher = PublishSubject<Bool>()
-    let friendPublisher = PublishSubject<Friend>()
-    let popupPublisher = PublishSubject<Void>()
-    let showLoadingPublisher = PublishSubject<Bool>()
-    let showAlertsPublisher = PublishSubject<(String, String)>()
     
     init(userService: UserServiceProtocol,
          userDefaults: UserDefaultsProtocol) {
         self.userService = userService
         self.userDefaults = userDefaults
         
-        input = Input(tapFavorite: tapFavoritePublisher.asObserver(),
-                      tapDelete: tapDeletePublisher.asObserver(),
-                      tapWrite: tapWritePublisher.asObserver())
-        output = Output(enableFavorite: enableFavoritePublisher,
-                        friend: friendPublisher,
-                        popup: popupPublisher,
-                        showLoading: showLoadingPublisher,
-                        showAlerts: showAlertsPublisher)
         super.init()
         
-        tapFavoritePublisher.withLatestFrom(Observable.combineLatest(friendPublisher, enableFavoritePublisher)).bind {[weak self] (friend, isFavorite) in
+        input.tapFavorite.withLatestFrom(Observable.combineLatest(output.friend, output.enableFavorite)).bind {[weak self] (friend, isFavorite) in
             guard let self = self else { return }
             self.userService.favoriteFriend(userId: self.userDefaults.getUserToken(), friendId: friend.id, isFavorite: !isFavorite)
-            self.enableFavoritePublisher.onNext(!isFavorite)
+            self.output.enableFavorite.accept((!isFavorite))
         }.disposed(by: disposeBag)
         
-        tapDeletePublisher.withLatestFrom(friendPublisher).bind { [weak self] (friend) in
+        input.tapDelete.withLatestFrom(output.friend).bind { [weak self] (friend) in
             guard let self = self else { return }
-            self.showLoadingPublisher.onNext(true)
+            self.output.showLoading.accept(true)
             self.userService.deleteFriend(userId: self.userDefaults.getUserToken(), friendId: friend.id) { (observable) in
                 observable.subscribe(onNext: { (_) in
-                    self.showLoadingPublisher.onNext(false)
-                    self.popupPublisher.onNext(())
+                    self.output.showLoading.accept(false)
+                    self.output.popup.accept(())
                 }, onError: { (error) in
-                    self.showAlertsPublisher.onNext(("친구 삭제 오류", error.localizedDescription))
-                    self.showLoadingPublisher.onNext(false)
+                    self.output.showAlerts.accept(("친구 삭제 오류", error.localizedDescription))
+                    self.output.showLoading.accept(false)
                 }).disposed(by: self.disposeBag)
             }
         }.disposed(by: disposeBag)
+        
+        input.tapWrite.withLatestFrom(output.friend).bind(to: output.goToWrite)
+            .disposed(by: disposeBag)
     }
     
     func fetchFriend(friendId: String) {
-        self.showLoadingPublisher.onNext(true)
+        self.output.showLoading.accept(true)
         userService.findFriend(userId: self.userDefaults.getUserToken(), friendId: friendId) { [weak self] (friendObservable) in
             guard let self = self else { return }
             friendObservable.subscribe(onNext: { (friend) in
-                self.friendPublisher.onNext(friend)
-                self.enableFavoritePublisher.onNext(friend.favorite)
-                self.showLoadingPublisher.onNext(false)
+                self.output.friend.accept(friend)
+                self.output.enableFavorite.accept(friend.favorite)
+                self.output.showLoading.accept(false)
             }, onError: { (error) in
                 if let error = error as? CommonError {
-                    self.showAlertsPublisher.onNext(("내 정보 조회 오류", error.description))
+                    self.output.showAlerts.accept(("내 정보 조회 오류", error.description))
                 } else {
-                    self.showAlertsPublisher.onNext(("내 정보 조회 오류", error.localizedDescription))
+                    self.output.showAlerts.accept(("내 정보 조회 오류", error.localizedDescription))
                 }
-                self.showLoadingPublisher.onNext(false)
+                self.output.showLoading.accept(false)
             }).disposed(by: self.disposeBag)
         }
     }
